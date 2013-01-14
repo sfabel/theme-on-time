ae996dcf000e3b79942a18365a87f1ad972c806c
Stephan Fabel <stephan.fabel@gmail.com>
2013-01-13 22:17:50 -1000

    Added documentation string.

--- theme-on-time.el
+++ theme-on-time.el
@@ -1,6 +1,6 @@
-
 ;; theme-on-time
 (defun theme-on-time ()
+  "Switches the theme according to time of day."
   (let ((ct (caddr (decode-time))))
     (cond ((and (> ct 0) (< ct 6)) (load-theme 'sanityinc-tomorrow-blue t))
 	  ((and (> ct 6) (< ct 12)) (load-theme 'sanityinc-tomorrow-day t))
