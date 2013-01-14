84c4701e9528bd60b583a16f2610ccae73d72c7a
Stephan <sfabel@hawaii.edu>
2013-01-13 22:05:39 -1000

    upload theme-on-time.el

---
+++ theme-on-time.el
@@ -0,0 +1,8 @@
+
+;; theme-on-time
+(defun theme-on-time ()
+  (let ((ct (caddr (decode-time))))
+    (cond ((and (> ct 0) (< ct 6)) (load-theme 'sanityinc-tomorrow-blue t))
+	  ((and (> ct 6) (< ct 12)) (load-theme 'sanityinc-tomorrow-day t))
+	  ((and (> ct 12) (< ct 18)) (load-theme 'sanityinc-tomorrow-bright t))
+	  ((and (> ct 18) (< ct 24)) (load-theme 'sanityinc-tomorrow-night t)))))
